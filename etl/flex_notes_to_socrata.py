f22873e6397055179c038a4622b9db23720f1b7e
Charlie Henry <thomashenry@utexas.edu>
2024-05-01 17:09:00 -0500

    311 reporting ETL

---
+++ etl/flex_notes_to_socrata.py
@@ -0,0 +1,94 @@
+"""
+Downloads flex question/answers for CSRs data from a CSV report endpoint and then uploads the data to a Socrata dataset
+"""
+
+import os
+import logging
+
+import pandas as pd
+from sodapy import Socrata
+
+from field_maps import FLEX_NOTES_MAP
+import utils
+
+# Socrata Secrets
+SO_WEB = os.getenv("SO_WEB")
+SO_TOKEN = os.getenv("SO_TOKEN")
+SO_KEY = os.getenv("SO_KEY")
+SO_SECRET = os.getenv("SO_SECRET")
+DATASET = os.getenv("FLEX_NOTES_DATASET")
+
+# CSR CSV data endpoint
+ENDPOINT = os.getenv("FLEX_NOTE_ENDPOINT")
+
+# Questions we do not want to store
+IGNORED_QUESTIONS = [
+    "Mobile Apps Reporter Information",
+    "Provide license plate number of vehicle, if known.",
+    "*What are the last 4 digits of the card number that you used for this transaction?",
+    "Enter APD  # if known (Case #, Officer #, etc.).",
+    "Provide User ID from ParkATX App (If user ID is unavailable, type N/A)",
+    "What are the last 4 digits of the card number that you used for this transaction?",
+    "What is the 9-digit citation number (top line)?",
+]
+
+
+def extract():
+    try:
+        df = pd.read_excel(ENDPOINT)
+    except UnicodeError as e:
+        logger.info("Unexpected file type returned from the report endpoint. Check that you are on the city network. "
+                    "It's likely that your request is getting flagged as a bot by the web app firewall.")
+        raise e
+    except Exception as e:
+        raise e
+    logger.info(f"Downloaded {len(df)} Flex Notes from endpoint")
+    return df
+
+
+def transform(df):
+    logger.info("Transforming Flex Notes")
+
+    # Field mapping
+    df = df[list(FLEX_NOTES_MAP.keys())]
+    df.rename(columns=FLEX_NOTES_MAP, inplace=True)
+
+    # Dropping unwanted questions from our dataset
+    df = df[~df["flex_question"].isin(IGNORED_QUESTIONS)]
+
+    # Replacing missing values with None instead of the default NaN pandas uses
+    df.replace({pd.NA: None}, inplace=True)
+
+    payload = df.to_dict("records")
+    return payload
+
+
+def load(client, data):
+    logger.info("Uploading CSR data to Socrata")
+    res = client.upsert(DATASET, data)
+    logger.info(res)
+    return res
+
+
+def main():
+    soda = Socrata(
+        SO_WEB,
+        SO_TOKEN,
+        username=SO_KEY,
+        password=SO_SECRET,
+        timeout=500,
+    )
+
+    data = extract()
+    data = transform(data)
+    res = load(soda, data)
+
+    return res
+
+
+if __name__ == "__main__":
+    logger = utils.get_logger(
+        __name__,
+        level=logging.INFO,
+    )
+    main()
--- readme.md
+++ readme.md
@@ -1,5 +1,47 @@
 # Transportation & Public Works 3-1-1 Reporting
 
-This repo contains python scripts that pull reports from the [City's 3-1-1 system](https://www.austintexas.gov/department/311). These reports contain information on what was reported by Customer Service Request (CSR) from the public and when/how the department took action to resolve it.
+This repo contains python scripts that pull CSV reports from the [City's 3-1-1 system](https://www.austintexas.gov/department/311). These reports contain information on what was reported by the Customer Service Request (CSR) from the public and when/how the department took action to resolve it. These reports are then stored in a City datahub dataset.
 
-work in progress...
+![data flow diagram showing the CSRs retrieved as CSV files then stored in the city datahub.](docs/data_flow.png)
+
+## CSRs to Socrata
+
+`csr_to_socrata.py` processes a report that contains CSR-level data on 3-1-1 service requests. There is one row per service request.
+
+`$ python etl/csr_to_socrata.py`
+
+## Flex Notes to Socrata
+
+"Flex notes" are questions that are asked based on the type of CSR that is submitted. Each CSR can have multiple flex questions/answers. `flex_notes_to_socrata.py` processes a report that contains the flex notes.
+
+`$ python etl/flex_notes_to_socrata.py`
+
+## Environment variables
+
+All required environment variables are in `env_template` and can be found in our password storage. 
+
+## Network
+
+Note that you also must be on the City network or you will likely not be able to retrieve the reports from 3-1-1. You will see this error:
+
+```
+Unexpected file type returned from the CSV endpoint. Check that you are on the city network. 
+
+It's likely that your request is getting flagged as a bot by the web app firewall.
+```
+
+## Docker
+
+This repo can be used with a docker container. You can either build it yourself with:
+
+`$ docker build . -t dts-311-reporting:production`
+
+or pull from our dockerhub account:
+
+`$ docker pull atddocker/dts-311-reporting:production`
+
+Then, provide the environment variables described in env_template to the docker image:
+
+`$ docker run -it --env-file env_file dts-311-reporting:production /bin/bash` 
+
+Then, provide the command you would like to run.
